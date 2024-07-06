24ecf0606cbc533d11bba7461b140172aea86dc5
Thanh Long <thanhlongnguyen140499@gmail.com>
2024-07-06 21:25:47 +0700

    Hackerrank - Datastructure & Print Linked list

---
+++ practices/LinkedLists/PrintLinkedList.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const readlineSync = require("readline-sync");
+
+function readLine() {
+  return readlineSync.question();
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+// Complete the printLinkedList function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function printLinkedList(head) {
+    // console.log("head vs head.next: ", head, head.next)
+    if (head !== null) {
+        console.log(head.data);
+    } 
+    if (head.next !== null) {
+        printLinkedList(head.next);
+    }
+}
+
+function main() {
+    const llistCount = parseInt(readLine(), 10);
+
+    let llist = new SinglyLinkedList();
+
+    for (let i = 0; i < llistCount; i++) {
+        const llistItem = parseInt(readLine(), 10);
+        llist.insertNode(llistItem);
+    }
+
+    printLinkedList(llist.head);
+}
+
+main();
