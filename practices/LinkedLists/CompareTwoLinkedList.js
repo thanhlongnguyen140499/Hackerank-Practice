24cb1073495a3ed0d0bfced1e30e652e080b29a0
Edward <edward@enclave.vn>
2024-08-07 11:00:45 +0700

    Hackerrank - Datastructure & LinkedList Compare Two Linked List

---
+++ practices/LinkedLists/CompareTwoLinkedList.js
@@ -0,0 +1,106 @@
+'use strict';
+
+const fs = require('fs');
+
+const readlineSync = require("readline-sync");
+
+function readLine() {
+  return readlineSync.question();
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+
+// Complete the CompareLists function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function CompareLists(llist1, llist2) {
+    while(llist1){
+        if(!llist2 || llist1.data !== llist2.data){
+            return 0
+        }
+        llist1 = llist1.next
+        llist2 = llist2.next
+        
+    }
+    return 1;
+}
+
+function main() {
+    const filePath = "/Users/edwardnguyen/Documents/Edward/Interview/node-hello-world/output.txt";
+    const ws = fs.createWriteStream(filePath);
+
+    const tests = parseInt(readLine(), 10);
+
+    for (let testsItr = 0; testsItr < tests; testsItr++) {
+        const llist1Count = parseInt(readLine(), 10);
+
+        let llist1 = new SinglyLinkedList();
+
+        for (let i = 0; i < llist1Count; i++) {
+            const llist1Item = parseInt(readLine(), 10);
+            llist1.insertNode(llist1Item);
+        }
+      
+      	const llist2Count = parseInt(readLine(), 10);
+
+        let llist2 = new SinglyLinkedList();
+
+        for (let i = 0; i < llist2Count; i++) {
+            const llist2Item = parseInt(readLine(), 10);
+            llist2.insertNode(llist2Item);
+        }
+
+        let result = CompareLists(llist1.head, llist2.head);
+        console.log("result: ", result);
+
+        ws.write((result ? 1 : 0) + "\n");
+    }
+
+    ws.end();
+}
+
+main()
